e926581e1fec0badc9cd9116d9f792b0a47f444c
Snigdhendu-Chhotaray-28 <snigdhenduchhotaray@gmail.com>
2026-01-29 23:32:54 +0530

    use startTime fn

--- dist/script.js
+++ dist/script.js
@@ -3,4 +3,11 @@ let hour_hand = document.querySelector('.hour-hand');
 let minute_hand = document.querySelector('.minute-hand');
 let second_hand = document.querySelector('.second-hand');
 
+function startTimer() {
+    const intervalId = setInterval(() => {
+        
+        
+    }, 1000);
+}
+startTimer();
 
