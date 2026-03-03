0113b7db4a86a91f56764c3ee0c16458d2a6e399
Snigdhendu-Chhotaray-28 <snigdhenduchhotaray@gmail.com>
2026-03-03 23:15:57 +0530

    else statement

--- dist/script.js
+++ dist/script.js
@@ -307,7 +307,11 @@ window.addEventListener('load',()=>{
                         if(el.innerText==correntTime.getHours()){
                             el.classList.add('high_light');
                         }
-                        
+                        else{
+                            if(el.classList.contains('high_light')){
+                                el.classList.remove('high_light');
+                            }
+                        }
                     }
 
                 });
